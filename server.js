8e798fe01d4deb57219692440f6dab05945bbdd1
prem <softwareengineerprem@gmail.com>
2022-03-31 00:40:41 +0530

    ghhh

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var path = require('path');
 
-const app = express();
+var app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static(__dirname + '/dist/<emedi-application>'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<emedi-application>/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
