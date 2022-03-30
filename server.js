ee3cd95117a7bc1bc410665276b76f3028d3dd15
prem <softwareengineerprem@gmail.com>
2022-03-31 00:57:44 +0530

    ddd

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(__dirname + '/dist/emedi-application'));
+app.use(express.static(__dirname + '/emedi-application'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/emedi-application/index.html'));
+res.sendFile(path.join(__dirname+'/emedi-application/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function(){
